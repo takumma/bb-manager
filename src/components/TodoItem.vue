75a5aecdcc16db65bd2c272e1f25da0e6fcde056
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-04-02 15:14:11 +0900

    賞味期限によって色がつくようにした。

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,5 @@
 <template>
-	<li class="list-complete-item">
+	<li class="list-complete-item" :class="isExpired(bbDays)">
     <label v-bind:class="{ done: isChecked }">
       <input type="checkbox" v-model="childisChecked" v-on:change="deleteCheck">
       {{ title + " 賞味期限:" + bb + "(あと" + bbDays + "日)"}}
@@ -14,13 +14,33 @@ export default {
   data: function(){
     return {
       childisChecked: this.isChecked,  //v-modelでisCheckedを指定できないため追加
-      bbDays: (moment(this.bb).diff(moment(), 'days') + 1)
+      bbDays: (moment(this.bb).diff(moment(), 'days')),
+
+      // isExpired
+      ClassDanger: 'danger',
+      ClassWarn: 'warn'
     }
   },
   methods: {
     deleteCheck: function(){
       this.$emit('delete', this.childisChecked, this.id);
+    },
+    isExpired: function(day) {
+      if(day < 0) {
+        return this.ClassDanger;
+      } else if(day <= 5) {
+        return this.ClassWarn;
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+.danger {
+  background-color: rgba(255, 0, 0, 0.5);
+}
+.warn {
+  background-color: rgba(255, 255, 0.5);
+}
+</style>
